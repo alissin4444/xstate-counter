e84c126e5485814757603606b2093945b02eb829
alissin4444 <dev.alissonsantos@gmail.com>
2020-11-24 22:08:12 -0300

    aplication finalized

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,37 @@
 import { useMachine } from "@xstate/react";
+import { useEffect, useState } from "react";
 
 import counterMachine from "../store/xstate/counterMachine";
 
 export default function Home() {
-  const [counterMSState, counterMSStateSend] = useMachine(counterMachine);
+  const [machine, send] = useMachine(counterMachine);
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    if (machine.value === "active") {
+      const timeout = setTimeout(() => {
+        setCounter(counter + 1);
+      }, 1000);
+      return () => {
+        clearTimeout(timeout);
+      };
+    }
+  }, [counter, machine.value]);
 
   return (
     <div>
       <h1>Counter Machine</h1>
       <hr />
       <p>
-        <i>Your current state is {counterMSState.value}</i>
+        <i>Your current machine state is {machine.value}</i>
+      </p>
+      <p>
+        <i>And</i>, your current counter state is <b>{counter}</b>
       </p>
 
-      <button onClick={() => counterMSStateSend("TOGGLE")}>
-        {counterMSState.value === "inactive"
+      <button onClick={() => send("TOGGLE")}>
+        {machine.value === "inactive"
           ? "Click to activate"
           : "Active! Click to deactivate"}
       </button>
